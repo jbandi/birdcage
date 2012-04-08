7ae3e2d8f5d016768d50f96fde957b0189a59c5a
jbandi <dev@jonasbandi.net>
2012-04-08 23:14:16 +0200

    Adding background worker

---
+++ app/clock.rb
@@ -0,0 +1,51 @@
+require 'rubygems'
+require_relative '../config/boot'
+require_relative '../config/environment'
+
+every(10.minute, 'Tweet Sender') {
+  
+  log "Starting scheduling round..."
+  
+  activeUsers = User.where("approved = ? and active = ?", true, true)
+  
+  log "Active user count: " + activeUsers.count.to_s
+
+  activeUsers.each do |user|
+    
+    user.reload()
+    
+    if user.last_post != nil && user.last_post + user.post_interval.minutes > Time.now 
+      min_unitl_next_tweet = (user.last_post.advance(:minutes => user.post_interval) - Time.now)/60
+      log user.name + ": Its not yet time for the next tweet. Waiting another " + min_unitl_next_tweet.to_s + " minutes."
+      
+    else  
+    
+      log user.name + ": scheduling next tweet."
+    
+      next_tweet = user.tweets.where("sent = ?", false).order("priority desc, created_at asc").first
+    
+      if next_tweet
+        Twitter.configure do |config|
+          # consumer key/secret are configured in config/initializers/twitter.rb
+          config.oauth_token = user.token
+          config.oauth_token_secret = user.secret
+        end
+      
+        client = Twitter::Client.new
+        client.update next_tweet.content
+
+        log user.name + ": sent a tweet -> " + next_tweet.content[0..30] + "..." 
+
+        next_tweet.sent = true;
+        next_tweet.save
+        user.last_post = Time.now
+        user.save
+      else
+        log user.name + ": no tweets available."
+      end
+      
+      log user.name + ": finished tweet scheduling."
+    end
+    
+  end  
+}
