0ebddb63f925dff6fd06c8134c38dfbbfdd4c0b6
jbandi <dev@jonasbandi.net>
2012-07-30 23:43:35 +0200

    Showing latest tweet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,9 @@ class HomeController < ApplicationController
     respond_to do |format|
       format.html do
         @tweet = Tweet.new
-        @tweets = current_user.tweets.where("sent = ?", false).order("priority desc, created_at asc") unless !current_user
+#        @tweets = current_user.tweets.where("sent = ?", false).order("priority desc, created_at asc") unless !current_user
+        @tweets = current_user.unsent_tweets unless !current_user
+        @latest_tweet_content = current_user.latest_tweet_content
       end
       format.js do
         @bitly_login = current_user.bitly_login
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
     end
   end
 
-  def last_post_timestamp
+  def latest_tweet_time
     if last_post 
       last_post.to_s(:short) 
     else
@@ -19,11 +19,24 @@ class User < ActiveRecord::Base
     end
   end
 
-  def next_post
+  def next_tweet_time
     if last_post && last_post + post_interval.minutes - Time.now > 0
      last_post + post_interval.minutes
     else
       Time.now
+    end
+  end
+
+  def unsent_tweets
+    tweets.where("sent = ?", false).order("priority desc, created_at asc")
+  end
+
+  def latest_tweet_content
+    t = tweets.where("sent = ?", true).order("updated_at desc").first
+    if t
+      t.content
+    else
+      ""
     end
   end
 
